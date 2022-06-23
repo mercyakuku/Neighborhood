a7b9c3e76458b91f75b97745190346abe0e678c2
mercyakuku <mercyakuku24@gmail.com>
2022-06-23 03:25:13 +0300

    Added imports to models file

--- neighbors/models.py
+++ neighbors/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 # Create your models here.
+
+
