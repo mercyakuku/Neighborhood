26103334730f1b13d304df6c0e145427ce331c4a
mercyakuku <mercyakuku24@gmail.com>
2022-06-23 03:21:52 +0300

    Added forms classes to display

--- neighbors/forms.py
+++ neighbors/forms.py
@@ -4,3 +4,36 @@ from django.contrib.auth.models import User
 from .models import Profile, NeighbourHood, Business, Post
 from cloudinary.models import CloudinaryField
 
+# Create your forms here
+
+class SignupForm(UserCreationForm):
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+
+class UpdateProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ('user', 'neighbourhood')
+
+
+class NeighbourHoodForm(forms.ModelForm):
+    class Meta:
+        model = NeighbourHood
+        exclude = ('admin',)
+
+
+class BusinessForm(forms.ModelForm):
+    class Meta:
+        model = Business
+        exclude = ('user', 'neighbourhood')
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        exclude = ('user', 'hood')
+
